f837e7e8d5ee534dbb57de7f9c8ccaed273afbe2
Ethan MacDonald <e@ethanm.ca>
2026-03-15 22:50:04 -0300

    More premake updates

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
 workspace "foxtrot"
-    configurations { "Debug", "Release" }
+    configurations { "Debug", "DebugRelease", "Release" }
     targetdir "build/%{cfg.buildcfg}"
     cppdialect "C++20"
-	
-	if _ACTION and _ACTION:startswith("vs") then 
+
+	if _ACTION and _ACTION:startswith("vs") then
 		platforms {"x64"}
 	else
 		platforms {"x64", "macOS"}
@@ -20,35 +20,49 @@ workspace "foxtrot"
         architecture "aarch64"
         buildoptions {"-Wno-nullability-completeness"}
         libdirs { "./Lib/macOS" }
-	
-	
+
+
     filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
 
+    filter "configurations:DebugRelease"
+        defines { "NDEBUG" }
+        symbols "On"
+        optimize "On"
+
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
-	
+
 	filter {}
-	
+
     newoption {
         trigger = "simde",
         description = "Use SIMDe to use AVX on a non-AVX platform"
     }
-	
-	
+
+
 project "foxtrot"
     kind "ConsoleApp"
     language "C++"
     targetdir "build/%{cfg.buildcfg}"
 	objdir "buildobj/%{cfg.buildcfg}"
-	libdirs {"$(VULKAN_SDK)/Lib"}
-  
+
+	if os.host() == "windows" then
+	    libdirs {"$(VULKAN_SDK)/Lib"}
+        links {"vulkan-1"}
+		includedirs {"$(VULKAN_SDK)/include"}
+	else
+	    libdirs {"$VULKAN_SDK/lib"}
+		links {"vulkan.1"}
+		includedirs {"$VULKAN_SDK/include"}
+	end
+
     files {"Src/**.cpp", "Src/**.inl", "Src/**.hpp", "Src/ThirdParty/**.cpp", "Src/ThirdParty/**.h", "Src/ThirdParty/**.hpp", "Src/ThirdParty/**.inl"}
 
-    includedirs { "Src", "Src/ThirdParty", "Lib/Include", "Lib/Include", "$(VULKAN_SDK)/include" }
-    links {"vulkan-1", "slang", "turbojpeg", "SDL3"}
+    includedirs { "Src", "Src/ThirdParty", "Lib/Include", "Lib/Include" }
+    links {"slang", "turbojpeg", "SDL3"}
 
     defines { "FX_BASE_DIR=\"" .. _MAIN_SCRIPT_DIR .. "\"", "_USE_MATH_DEFINES" }
 
